d85a010e7b14bc693e2763a68e0529811c46afb3
Purav Manot <manot.purav@gmail.com>
2021-11-09 01:19:11 +0530

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Purav Manot on 09/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/GenerativeArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerativeArtApp.swift
+//  Shared
+//
+//  Created by Purav Manot on 09/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GenerativeArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
